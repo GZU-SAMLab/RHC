04693dd580eaabaa56b44b24acdc1882232d941c
SAMLab <147920838+GZU-SAMLab@users.noreply.github.com>
2025-07-01 18:26:25 +0800

    Delete trans_model.py

--- trans_model.py
+++
@@ -1,8 +0,0 @@
-import torch
-from test_main import Resnet50
-
-model = "/home/wcy/testnet/premodel/best/Resnet50_cifar100.pth"
-
-a = torch.load(model).cuda()
-print(a)
-torch.save(a.module,"Resnet_for_cifar100.pth")
